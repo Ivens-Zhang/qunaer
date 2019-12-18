81c6fc5a01420f0c2b85fd36b1250efd5e1677cd
Ivens_Zhang <49118158+Ivens-Zhang@users.noreply.github.com>
2019-12-18 20:59:42 +0800

    初始化 Detail 页面
    1. 横幅
    2. header
    3. 详情列表

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <detail-list></detail-list>
+  </div>
+</template>
+
+<script>
+import DetailBanner from './components/Banner.vue'
+import DetailHeader from './components/Header.vue'
+import DetailList from './components/List.vue'
+// import axios from 'axios'
+export default {
+  name: 'Home',
+  components: {
+    DetailBanner,
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
